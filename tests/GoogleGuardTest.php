22a53dc10a4966c1c3ecf477e00b4acc42ad9476
dusterio <denis@mysenko.com>
2017-04-10 16:18:01 +1000

    + Basic tests

---
+++ tests/GoogleGuardTest.php
@@ -0,0 +1,69 @@
+<?php
+
+class DummyUser {
+    public function getName()
+    {
+        return 'Denis';
+    }
+
+    public function getEmail()
+    {
+        return 'admin@.';
+    }
+
+    public $token = 'AAA';
+}
+
+class UserModel implements \Illuminate\Contracts\Auth\Authenticatable {
+    public function fill() {
+        return $this;
+    }
+
+    public function getAuthIdentifierName()
+    {
+    }
+
+    public function getAuthIdentifier()
+    {
+    }
+
+    public function getAuthPassword()
+    {
+    }
+
+    public function getRememberToken()
+    {
+    }
+
+    public function setRememberToken($value)
+    {
+    }
+
+    public function getRememberTokenName()
+    {
+    }
+
+    public $token = 'AAA';
+}
+
+
+/**
+ * Class GoogleGuardTest
+ */
+class GoogleGuardTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function user_is_logged_out_after_timeout()
+    {
+        $session = new \Illuminate\Session\Store('aaa', new SessionHandler());
+        $guard = new \Dusterio\LaravelGoogleGuard\GoogleGuard($session, 100, UserModel::class);
+        $guard->loginUsingSocialite(new DummyUser());
+        $this->assertTrue($guard->check());
+
+        $session->set('google_guard_signed_at', time() - 10000);
+        $this->assertTrue($guard->guest());
+        $this->assertFalse($session->has('google_guard_user'));
+    }
+}
